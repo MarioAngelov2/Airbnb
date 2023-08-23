6773f236c7d9c75d8710eb1392331dde0fcf0938
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-23 23:26:45 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Account from "./components/Account/Account"
 import Navbar from "./components/Navbar/Navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="py-3 px-4">
       <Navbar />
+      <Account />
     </div>
   )
 }
