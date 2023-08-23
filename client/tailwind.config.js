da955689b1700236632756e8f360113d58db52d5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-23 22:21:07 +0300

    Add custom themes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FE395C",
+            },
+            maxWidth: {
+                global: "1440px",
+            },
+        },
+    },
+    plugins: [],
+};
