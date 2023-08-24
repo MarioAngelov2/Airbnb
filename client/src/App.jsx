2c9f69164f3e3c8126b062bb817aa9c661fff762
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-24 21:54:20 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import Account from "./components/Account/Account"
-import Navbar from "./components/Navbar/Navbar"
+import { Route, Routes } from "react-router-dom";
+import Account from "./components/Account/Account";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
-  return (
-    <div className="py-3 px-4">
-      <Navbar />
-      <Account />
-    </div>
-  )
+    return (
+        <div className="py-3 px-4">
+                <Navbar />
+            <Routes>
+                <Route path="/account" element={<Account />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
