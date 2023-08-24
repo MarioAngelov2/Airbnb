5daf2d108f3f445ce20fb39e29d66fd75b7601d4
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-24 21:54:18 +0300

    Update AccountNav.jsx

--- client/src/components/Account/AccountNav.jsx
+++ client/src/components/Account/AccountNav.jsx
@@ -1,28 +1,52 @@
 import React from "react";
-import {useLocation} from 'react-router-dom'
+import { useLocation, Link, NavLink, useParams } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { FaRegBookmark } from "react-icons/fa";
 import { PiBuildings } from "react-icons/pi";
 
 function AccountNav() {
-    const location = useLocation();
+    const { pathname } = useLocation();
+    console.log(useLocation())
+    let subpage = pathname.split('/')?.[2]
+    
+    if (subpage === 'undefined') {
+        subpage = 'profile'
+    }
 
-    console.log(location)
+    console.log(subpage)
 
+    function linkClasses(type = null) {
+        let classes = "flex items-center gap-1"
+        if (type === subpage) {
+            classes += " bg-primary text-white rounded-full"
+        }
+
+        return classes
+    }
+  
     return (
         <div className="flex justify-center max-w-global mx-auto my-16 mb-8 gap-5">
-            <div className="flex items-center gap-1 cursor-pointer">
+            <NavLink
+                className={linkClasses('profile')}
+                to={"/account"}
+            >
                 <CgProfile size={20} />
                 My profile
-            </div>
-            <div className="flex items-center gap-1 cursor-pointer">
+            </NavLink>
+            <NavLink
+                className={linkClasses('bookings')}
+                to={"/account/bookings"}
+            >
                 <FaRegBookmark size={20} />
                 My bookings
-            </div>
-            <div className="flex items-center gap-1 cursor-pointer">
+            </NavLink>
+            <NavLink
+                className={linkClasses('places')}
+                to={"/account/places"}
+            >
                 <PiBuildings size={20} />
                 My accoommodations
-            </div>
+            </NavLink>
         </div>
     );
 }
