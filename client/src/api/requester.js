071475c1a469b095bce73075a2191d23af8e1346
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-26 22:08:33 +0300

    Create login api

--- client/src/api/requester.js
+++ client/src/api/requester.js
@@ -1,17 +1,26 @@
-const URL = 'http://localhost:5001'
+const URL = "http://localhost:5001";
 
 export async function register(data) {
     try {
         const response = await fetch(`${URL}/register`, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify(data)
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
         });
 
-        return response
+        return response;
     } catch (error) {
-        console.log(error)
+        console.log(error);
+    }
+}
+
+export async function login() {
+    try {
+        const response = await fetch(`${URL}/login`);
+        return response.json();
+    } catch (error) {
+        console.log(error);
     }
 }
