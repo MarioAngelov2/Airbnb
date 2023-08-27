9a09dd51ca07f22490884321be7fb06c817bfe0a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 10:43:50 +0300

    Create login post request

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import * as api from "../../api/requester";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (ev) => {
+        ev.preventDefault();
+
+        const data = { email, password };
+        await api.login(data);
+    };
+
     return (
         <div className="max-w-global mx-auto flex justify-center mt-20">
             <div className="flex flex-col">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md" action="">
-                    <input type="email" placeholder="your@email.com" />
-                    <input type="password" placeholder="password" />
+                <form
+                    onSubmit={handleSubmit}
+                    className="max-w-md"
+                    action="POST"
+                >
+                    <input
+                        type="email"
+                        placeholder="your@email.com"
+                        value={email}
+                        onChange={(ev) => setEmail(ev.target.value)}
+                    />
+                    <input type="password" placeholder="password" 
+                      value={password}
+                      onChange={(ev) => setPassword(ev.target.value)}/>
                     <button className="primary mt-5">Login</button>
                 </form>
                 <div className="text-center mt-4">
-                    Already have an account? <Link className="font-semibold underline" to={"/login"}>Login</Link>
+                    Already have an account?{" "}
+                    <Link className="font-semibold underline" to={"/login"}>
+                        Login
+                    </Link>
                 </div>
             </div>
         </div>
