5b9733fe32b9bde36ee33a6876df157162d210dc
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 14:23:22 +0300

    Create context for providing user credentials

---
+++ client/src/context/UserContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import * as api from "../api/requester";
+
+export const UserContext = createContext();
+
+export function UserContextProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [ready, setReady] = useState(false);
+
+    async function getUserCredentials() {
+        const response = await api.getUser();
+        setUser(response);
+        setReady(true);
+    }
+
+    useEffect(() => {
+        getUserCredentials();
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ user, setUser, ready }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export default UserContextProvider
