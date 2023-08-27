2007f777a528b0c24fa70486bb2c90c86c5b073e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 14:22:09 +0300

    Conditional navigate if user logged in

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { PiPaperPlane } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaRegUserCircle } from "react-icons/fa";
 import { BiSearchAlt2 } from "react-icons/bi";
+import { UserContext } from "../../context/userContext";
 
 function Navbar() {
+    const { user } = useContext(UserContext);
+
     return (
-        <Link to={'/'} className="flex justify-between max-w-global mx-auto">
+        <Link to={"/"} className="flex justify-between max-w-global mx-auto">
             <div className="flex items-center gap-1">
                 <PiPaperPlane className="text-primary" size={30} />
                 <h1 className="text-2xl font-bold text-primary">airbnb</h1>
@@ -19,12 +22,19 @@ function Navbar() {
                 <div>Any week</div>
                 <div className="border-l border-gray-300">&nbsp;</div>
                 <div>Add Guests</div>
-                <a href="/search" className="border rounded-3xl p-2 bg-primary text-white">
-                    <BiSearchAlt2 size={18} aria-label="Search Icon"/>
+                <a
+                    href="/search"
+                    className="border rounded-3xl p-2 bg-primary text-white"
+                >
+                    <BiSearchAlt2 size={18} aria-label="Search Icon" />
                 </a>
             </div>
             {/* third section */}
-            <Link to={'/account'} aria-label="User" className="flex gap-3 border rounded-3xl py-2 px-5 shadow-md shadow-gray-100">
+            <Link
+                to={user ? "/account" : "/login"}
+                aria-label="User"
+                className="flex gap-3 border rounded-3xl py-2 px-5 shadow-md shadow-gray-100"
+            >
                 <RxHamburgerMenu size={24} />
                 <FaRegUserCircle size={24} />
             </Link>
