d7d8f6973e948545c105f1f8782ec3739c9a6dc5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 10:44:03 +0300

    Update places.js

--- server/src/routes/places.js
+++ server/src/routes/places.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const createUser = require("../controllers/register");
+const loginUser = require("../controllers/login");
 
 const router = Router();
 
 router.post("/register", createUser);
-
+router.post("/login", loginUser);
 
 module.exports = router;
