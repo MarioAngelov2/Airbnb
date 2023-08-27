1a7280332b4b2060e6acec709ee9853cf4630d48
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 10:43:35 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,12 @@ import Account from "./components/Account/Account";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Header from "./components/Header/Header";
 
 function App() {
     return (
         <div className="py-3 px-4">
             <Navbar />
             <Routes>
-                <Route path="/" element={<Header />} />
                 <Route path="/account" element={<Account />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
