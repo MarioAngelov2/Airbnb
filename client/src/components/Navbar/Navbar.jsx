5e37b58ded707bc071d4b9da6db1301bcc5cc31f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 15:21:32 +0300

    Minor refactor

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,35 +10,34 @@ function Navbar() {
     const { user } = useContext(UserContext);
 
     return (
-        <Link to={"/"} className="flex justify-between max-w-global mx-auto">
-            <div className="flex items-center gap-1">
-                <PiPaperPlane className="text-primary" size={30} />
-                <h1 className="text-2xl font-bold text-primary">airbnb</h1>
-            </div>
-            {/* second section */}
-            <div className="hidden md:flex items-center gap-3 border rounded-full px-6 py-1 shadow-md shadow-gray-200">
-                <div>Anywhere</div>
-                <div className="border-l border-gray-300">&nbsp;</div>
-                <div>Any week</div>
-                <div className="border-l border-gray-300">&nbsp;</div>
-                <div>Add Guests</div>
-                <a
-                    href="/search"
-                    className="border rounded-3xl p-2 bg-primary text-white"
+        <div>
+            <div className="flex justify-between max-w-global mx-auto">
+                <Link to={"/"} className="flex items-center gap-1">
+                    <PiPaperPlane className="text-primary" size={30} />
+                    <h1 className="text-2xl font-bold text-primary">airbnb</h1>
+                </Link>
+                {/* second section */}
+                <div className="hidden md:flex items-center gap-3 border rounded-full px-6 py-1 shadow-md shadow-gray-200">
+                    <div>Anywhere</div>
+                    <div className="border-l border-gray-300">&nbsp;</div>
+                    <div>Any week</div>
+                    <div className="border-l border-gray-300">&nbsp;</div>
+                    <div>Add Guests</div>
+                    <div className="border rounded-3xl p-2 bg-primary text-white">
+                        <BiSearchAlt2 size={18} />
+                    </div>
+                </div>
+                {/* third section */}
+                <Link
+                    to={user ? "/account" : "/login"}
+                    aria-label="User"
+                    className="flex gap-3 border rounded-3xl py-2 px-5 shadow-md shadow-gray-100"
                 >
-                    <BiSearchAlt2 size={18} aria-label="Search Icon" />
-                </a>
+                    <RxHamburgerMenu size={24} />
+                    <FaRegUserCircle size={24} />
+                </Link>
             </div>
-            {/* third section */}
-            <Link
-                to={user ? "/account" : "/login"}
-                aria-label="User"
-                className="flex gap-3 border rounded-3xl py-2 px-5 shadow-md shadow-gray-100"
-            >
-                <RxHamburgerMenu size={24} />
-                <FaRegUserCircle size={24} />
-            </Link>
-        </Link>
+        </div>
     );
 }
 
