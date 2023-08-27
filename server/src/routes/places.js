a464ca62f66215f95802969ef3be70f03b9fe94b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 15:44:47 +0300

    Add logout route

--- server/src/routes/places.js
+++ server/src/routes/places.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const createUser = require("../controllers/register");
 const loginUser = require("../controllers/login");
 const getUser = require("../controllers/getUser");
+const logout = require("../controllers/logout");
 
 const router = Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/profile", getUser);
+router.get("/logout", logout);
 
 module.exports = router;
