f4fac72b8de07e50e29ad167669544e8a10b6e2e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 14:21:46 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,23 @@ import Account from "./components/Account/Account";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Places from "./components/Places/Places";
+import UserContextProvider from "./context/userContext";
+import axios from "axios";
 
 function App() {
     return (
-        <div className="py-3 px-4">
-            <Navbar />
-            <Routes>
-                <Route path="/account" element={<Account />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </div>
+        <UserContextProvider>
+            <div className="py-3 px-4">
+                <Navbar />
+                <Routes>
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/account" element={<Account />} />
+                    <Route path="/account/places" element={<Places />} />
+                </Routes>
+            </div>
+        </UserContextProvider>
     );
 }
 
