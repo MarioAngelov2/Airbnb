604c38229173c1ae3649b635fca9cff0f9023a7b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 14:38:10 +0300

    Add spinner & conditional render

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import AccountNav from './AccountNav'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AccountNav from "./AccountNav";
+import { UserContext } from "../../context/userContext";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Account() {
-  return (
-    <div>
-      <AccountNav />
-    </div>
-  )
+    const { user, ready } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    if (!ready) {
+        return (
+            <div className="flex flex-col items-center justify-center mt-32">
+                <ClipLoader className="mb-4"/>
+                <span>Loading...</span>
+            </div>
+        );
+    }
+
+    if (ready && !user) {
+        return navigate("/login");
+    }
+
+    return (
+        <div>
+            <AccountNav />
+            <div className="flex flex-col items-center justify-center mt-16">
+                <h1 className="text-3xl mb-1">Welcome {user.name}</h1>
+                <p>{user.email}</p>
+                <button className="primary max-w-md mt-4">Logout</button>
+            </div>
+        </div>
+    );
 }
 
-export default Account
+export default Account;
