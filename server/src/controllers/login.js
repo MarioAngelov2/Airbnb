2e8d080698c52cd248ee671ab03d6d3695c72d5c
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 10:44:00 +0300

    Create login controller

---
+++ server/src/controllers/login.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const createHttpError = require("http-errors");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).exec();
+
+    if (user) {
+        const correctPass = bcrypt.compare(password, user.password);
+
+        if (correctPass) {
+            jwt.sign(
+                { email: user.email, id: user._id },
+                process.env.JWT_SECRET,
+                {},
+                (err, token) => {
+                    if (err) throw err;
+                    res.cookie("token", token).json(user);
+                }
+            );
+        } else {
+            res.status(422).json("Invalid password or email address");
+        }
+    } else {
+        res.status(422).json("Invalid password or email address");
+    }
+};
+
+module.exports = loginUser;
