d541c7ce554fb33830a02d401070575a811ca105
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-27 10:44:16 +0300

    Add cookie parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const router = require("./src/routes/places");
 const { default: mongoose } = require("mongoose");
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 
