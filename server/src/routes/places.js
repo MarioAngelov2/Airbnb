fd856239f7a9322f6e9c24f59301ec048119d3e9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-28 23:19:34 +0300

    Refactor

--- server/src/routes/places.js
+++ server/src/routes/places.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
+const path = require("path");
+const multer = require("multer");
 const createUser = require("../controllers/register");
 const loginUser = require("../controllers/login");
 const getUser = require("../controllers/getUser");
 const logout = require("../controllers/logout");
 const uploadByLink = require("../controllers/UploadPhotos/UploadByLink");
+const uploadPhotoFromDevice = require("../controllers/UploadPhotos/UploadFromDevice");
+
+const parentDirectory = path.resolve(__dirname, "..");
+const PATH_TO_UPLOADS = path.join(
+    parentDirectory,
+    "/controllers/UploadPhotos/uploads"
+);
+
+const photosMiddleware = multer({ dest: PATH_TO_UPLOADS });
 
 const router = Router();
 
@@ -12,5 +23,10 @@ router.post("/login", loginUser);
 router.get("/profile", getUser);
 router.get("/logout", logout);
 router.post("/upload-by-link", uploadByLink);
+router.post(
+    "/upload",
+    photosMiddleware.array("photos", 10),
+    uploadPhotoFromDevice
+);
 
 module.exports = router;
