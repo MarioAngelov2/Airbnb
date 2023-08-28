df940cda4525683f2e8de46478693db49bb3f0a9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-28 23:19:25 +0300

    Create controller for uploading photos from device

---
+++ server/src/controllers/UploadPhotos/UploadFromDevice.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const uploadPhotoFromDevice = async (req, res) => {
+    const uploadedFiles = [];
+    for (let i = 0; i < req.files.length; i++) {
+        const { path, originalname } = req.files[i];
+        const parts = originalname.split(".");
+        console.log(parts)
+        const extension = parts[parts.length - 1];
+        const newPath = path + "." + extension;
+        fs.renameSync(path, newPath);
+        uploadedFiles.push(
+            newPath.replace(
+                "/Users/marioangelov/Desktop/Airbnb/server/src/controllers/UploadPhotos/uploads",
+                ""
+            )
+        );
+    }
+    res.json(uploadedFiles);
+};
+
+module.exports = uploadPhotoFromDevice;
