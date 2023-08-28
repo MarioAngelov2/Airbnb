7c8af305adeb1aa48359a4d26fc92cabf3161b83
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-28 20:29:22 +0300

    Add route for upload photos via link

--- server/src/routes/places.js
+++ server/src/routes/places.js
@@ -3,6 +3,7 @@ const createUser = require("../controllers/register");
 const loginUser = require("../controllers/login");
 const getUser = require("../controllers/getUser");
 const logout = require("../controllers/logout");
+const uploadByLink = require("../controllers/UploadPhotos/UploadByLink");
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/profile", getUser);
 router.get("/logout", logout);
+router.post("/upload-by-link", uploadByLink);
 
 module.exports = router;
