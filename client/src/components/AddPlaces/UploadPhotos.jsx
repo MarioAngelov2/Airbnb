6f17a2ea2e76120b82f82aed41359e476363c237
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-29 11:12:49 +0300

    Display images when upload in user panel

--- client/src/components/AddPlaces/UploadPhotos.jsx
+++ client/src/components/AddPlaces/UploadPhotos.jsx
@@ -17,6 +17,7 @@ function UploadPhotos({ uploadPhotos, setUploadPhotos }) {
     async function uploadFromDevice(ev) {
         const files = ev.target.files;
         const data = new FormData();
+        console.log(ev.target.files);
 
         for (let i = 0; i < files.length; i++) {
             data.append("photos", files[i]);
@@ -27,7 +28,6 @@ function UploadPhotos({ uploadPhotos, setUploadPhotos }) {
             setUploadPhotos((prev) => {
                 return [...prev, ...response];
             });
-            setUploadPhotos("");
         } catch (error) {
             console.log(error);
         }
@@ -49,7 +49,17 @@ function UploadPhotos({ uploadPhotos, setUploadPhotos }) {
                     Add&nbsp;photo
                 </button>
             </div>
-            <label className="flex h-32 mt-2 max-w-xs items-center justify-center cursor-pointer gap-1 border bg-transparent rounded-2xl text-gray-500">
+            <div className="grid gap-2 mt-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {uploadPhotos.length > 0 &&
+                    uploadPhotos.map((photo) => (
+                        <img
+                            className="rounded-2xl aspect-square object-cover"
+                            src={"http://localhost:5001/uploads/" + photo}
+                            alt=""
+                        />
+                    ))}
+            </div>
+            <label className="flex h-32 mt-2 max-w-lg md:max-w-xs items-center justify-center cursor-pointer gap-1 border bg-transparent rounded-2xl text-gray-500">
                 <AiOutlineCloudUpload size={38} />
                 Upload from device
                 <input
