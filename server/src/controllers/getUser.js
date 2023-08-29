df1e7d6cdf3ea25e99225ee68e9b182470b2e3b3
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-29 15:54:41 +0300

    Update getUser.js

--- server/src/controllers/getUser.js
+++ server/src/controllers/getUser.js
@@ -3,39 +3,36 @@ const createHttpError = require("http-errors");
 const jwt = require("jsonwebtoken");
 
 const getUser = async (req, res) => {
-    const {token} = req.cookies
+    try {
+        const { token } = req.cookies;
 
-    // try {
-    //     const { token } = req.cookies;
+        if (token) {
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            const userData = await User.findById(decodedToken.id);
 
-    //     if (token) {
-    //         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    //         console.log(decodedToken)
-    //         const userData = await User.findById(decodedToken._id);
+            if (!userData) {
+                throw createHttpError(404, 'User not found.')
+            }
 
-    //         if (!userData) {
-    //             throw createHttpError(404, 'User not found.')
-    //         }
-
-    //         const {name, email, id} = userData;
-    //         res.json({name, email, id})
-    //     } else {
-    //         res.json(null)
-    //     }
-    // } catch (error) {
-    //     console.log(error)
-    // }
-
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, {}, async (err, user) => {
-            if (err) throw err
-
-            const {name, email, id} = await User.findById(user.id);
+            const {name, email, id} = userData;
             res.json({name, email, id})
-        })
-    } else {
-        res.json(null)
+        } else {
+            res.json(null)
+        }
+    } catch (error) {
+        console.log(error)
     }
+
+    // if (token) {
+    //     jwt.verify(token, process.env.JWT_SECRET, {}, async (err, user) => {
+    //         if (err) throw err
+
+    //         const {name, email, id} = await User.findById(user.id);
+    //         res.json({name, email, id})
+    //     })
+    // } else {
+    //     res.json(null)
+    // }
 };
 
 module.exports = getUser;
