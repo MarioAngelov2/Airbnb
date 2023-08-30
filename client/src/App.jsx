a3ddf363afad1460952bedf7ccc71f8a69474498
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-30 10:55:45 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
+import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import Account from "./components/Account/Account";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Places from "./components/Places/Places";
+import UserPlaces from "./components/Places/UserPlaces";
 import AddPlaces from "./components/AddPlaces/AddPlaces";
 
 import UserContextProvider from "./context/userContext";
+import Home from "./components/Home/Home";
+
+axios.defaults.baseURL = "http://127.0.0.1:5001";
+axios.defaults.withCredentials = true;
 
 
 function App() {
@@ -15,11 +20,13 @@ function App() {
             <div className="py-3 px-4">
                 <Navbar />
                 <Routes>
+                    <Route path="/" element={<Home />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/account" element={<Account />} />
-                    <Route path="/account/places" element={<AddPlaces />} />
+                    <Route path="/account/places" element={<UserPlaces />} />
                     <Route path="/account/places/new" element={<AddPlaces />} />
+                    <Route path="/account/places/:id" element={<AddPlaces />} />
                 </Routes>
             </div>
         </UserContextProvider>
