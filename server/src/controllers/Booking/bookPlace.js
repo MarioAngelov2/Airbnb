d810b3a2a69785c670a557bb74eb3bf10e12780d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-31 11:53:37 +0300

    Create controller for booking place

---
+++ server/src/controllers/Booking/bookPlace.js
@@ -0,0 +1,31 @@
+const Booking = require("../../models/Booking");
+const Place = require("../../models/Place");
+const jwt = require("jsonwebtoken");
+
+const bookPlace = async (req, res) => {
+    const { id } = req.params;
+    const { token } = req.cookies;
+    const { checkIn, checkOut, guests, name, phone } = req.body;
+
+    if (!token) {
+        throw new Error("Not existing token");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.id;
+
+    const place = await Place.findById(id);
+
+    const booking = await Booking.create({
+        place: id,
+        checkIn,
+        checkOut,
+        guests,
+        name,
+        phone,
+    });
+
+    res.json(booking);
+};
+
+module.exports = bookPlace;
