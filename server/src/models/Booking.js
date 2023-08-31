52dad6891a605d4e49c7b128cde82f926ce1b405
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-31 18:23:26 +0300

    Add user's place & price to booking schema

--- server/src/models/Booking.js
+++ server/src/models/Booking.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema({
-    place: { type: mongoose.Schema.Types.ObjectId, required: true },
+    place: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Place",
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true },
     checkIn: { type: Date, required: true },
     checkOut: { type: Date, required: true },
     guests: { type: String, required: true },
     name: { type: String, required: true },
     phone: { type: String, required: true },
+    price: Number,
 });
 
 const BookingModel = mongoose.model("Booking", BookingSchema);
