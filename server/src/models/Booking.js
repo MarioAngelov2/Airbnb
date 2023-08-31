f88b75e17a64b41b41198b66a43b65fee274d774
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-08-31 11:53:46 +0300

    Minor refactor

--- server/src/models/Booking.js
+++ server/src/models/Booking.js
@@ -4,9 +4,9 @@ const BookingSchema = new mongoose.Schema({
     place: { type: mongoose.Schema.Types.ObjectId, required: true },
     checkIn: { type: Date, required: true },
     checkOut: { type: Date, required: true },
-    guests: { type: String, required },
-    name: { type: String, required },
-    phone: { type: String, required },
+    guests: { type: String, required: true },
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
 });
 
 const BookingModel = mongoose.model("Booking", BookingSchema);
