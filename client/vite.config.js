79f42adf69548162796df98fd8b0eda68ca4893c
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-13 13:22:51 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: "127.0.0.1",
+        port: 5173,
+    },
+});
