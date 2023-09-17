16fcae59548bab654ad643aea6ad26ef90cef4a2
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-17 19:58:50 +0300

    Update logout.js

--- server/src/controllers/logout.js
+++ server/src/controllers/logout.js
@@ -1,6 +1,15 @@
 const logout = (req, res, next) => {
     try {
         res.cookie("token", "", { expires: new Date(0) });
+
+        res.cookie("token", "", { expires: new Date(0), sameSite: "Lax" });
+
+        res.cookie("token", "", {
+            expires: new Date(0),
+            sameSite: "Lax",
+            secure: true,
+        });
+
         res.json("Logout successful");
     } catch (error) {
         next(error);
